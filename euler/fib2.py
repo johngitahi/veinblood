f34305c5e6dc180bbd6c8cd1fbc2de88dd1d4562
John Gitahi <gitahi109@gmail.com>
2023-12-16 20:25:37 +0300

    format fib2.py

--- euler/fib2.py
+++ euler/fib2.py
@@ -2,12 +2,13 @@
 # whose values do not exceed four million, find the sum
 # of the even-valued terms.
 
+
 def fibonacci_lt_limit(limit):
     a, b, sum = 1, 2, 0
 
     for _ in range(int(limit)):
         if b <= limit:
-            if (b % 2 == 0):
+            if b % 2 == 0:
                 sum += b
             a, b = b, (b + a)
 
